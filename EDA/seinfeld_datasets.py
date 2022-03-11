f8a9f1cccd73aec49ed66efc68f0bb183b04013f
WojtekBogucki <w.bogucki@student.mini.pw.edu.pl>
2022-03-11 20:41:27 +0100

    Update seinfeld data with removing characters, replacement and distinguishing

--- EDA/seinfeld_datasets.py
+++ EDA/seinfeld_datasets.py
@@ -1,39 +1,19 @@
-import pandas as pd
-import numpy as np
-import re
-import os
 from EDA.processing import *
 
 path = "../data/seinfeld"
 seinfeld_raw = pd.read_csv(f"{path}/seinfeld_lines_v2.csv", encoding="utf-8")
 
 # save speakers with over 100 lines
-top_speakers = filter_by_speakers(seinfeld_raw)
-top_speakers.to_csv(f"{path}/top_speakers.csv", index=False, encoding="utf-8")
-
-group_scenes = filter_group_scenes(top_speakers)
-group_scenes.head()
-group_scenes.to_csv(f"{path}/group_scenes.csv", index=False, encoding="utf-8")
-
-edges_weighted = get_speaker_network_edges(group_scenes)
-edges_weighted.head()
+edges_weighted = (seinfeld_raw.pipe(filter_by_speakers)
+                  .pipe(filter_group_scenes)
+                  .pipe(get_speaker_network_edges))
 edges_weighted.to_csv(f"{path}/edges_weighted.csv", index=False, encoding="utf-8")
 
 # top 30 speakers
-top_speakers = filter_by_speakers(seinfeld_raw, top=30)
-top_speakers.to_csv(f"{path}/top_speakers_top30.csv", index=False, encoding="utf-8")
-
-group_scenes = filter_group_scenes(top_speakers)
-group_scenes.head()
-group_scenes.to_csv(f"{path}/group_scenes_top30.csv", index=False, encoding="utf-8")
-
-edges_weighted = get_speaker_network_edges(group_scenes)
-edges_weighted.head()
+edges_weighted = (seinfeld_raw.pipe(filter_by_speakers, top=30)
+                  .pipe(filter_group_scenes)
+                  .pipe(get_speaker_network_edges))
 edges_weighted.to_csv(f"{path}/edges_weighted_top30.csv", index=False, encoding="utf-8")
 
 save_seasons(seinfeld_raw, path=path, count=20)
 save_episodes(seinfeld_raw, path=path, count=0)
-
-
-episode_edges = (seinfeld_raw[(seinfeld_raw.season==6) & (seinfeld_raw.episode==14)].pipe(filter_by_speakers, count=0)
-                             .pipe(filter_group_scenes))
--- EDA/seinfeld_eda.py
+++ EDA/seinfeld_eda.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from EDA.processing import fix_names, split_characters
+from EDA.processing import fix_names, split_characters, remove_speakers, fix_filtered_names, distinguish_characters
 
 pd.options.display.max_columns = 10
 pd.options.display.max_rows = None
@@ -15,24 +15,108 @@ len(seinfeld_df.speaker[seinfeld_df.speaker.str.contains(" & ")])
 len(seinfeld_df.speaker[seinfeld_df.speaker.str.contains("&")])
 seinfeld_df[seinfeld_df.speaker.apply(len) > 20]
 
-seinfeld_df.groupby("speaker").size().reset_index(name="count").sort_values("speaker", ascending=True)
+seinfeld_df.groupby("speaker").size().reset_index(name="count").sort_values("speaker", ascending=False)
 
 seinfeld_df = split_characters(seinfeld_df, [" and "," & ", "&"])
+
+speakers_to_remove = ["all",
+                      "all four men",
+                      "all three",
+                      "audience",
+                      "both",
+                      "building a",
+                      "building b",
+                      "building c",
+                      "clerk's voice",
+                      "computer voice",
+                      "crowd",
+                      "dj on radio",
+                      "elaine thinking",
+                      "elaine's brain",
+                      "elaine's voice",
+                      "everybody",
+                      "everyone",
+                      "george's voice",
+                      "group",
+                      "high pitched voice",
+                      "hospital voiceover",
+                      "jerry's answering machine",
+                      "jerry's outgoing message",
+                      "jerry's stand-up",
+                      "montage",
+                      "off stage",
+                      "opening monolog",
+                      "opening scene",
+                      "radio announcer",
+                      "sam's voice",
+                      "so far",
+                      "tv",
+                      "tv announcer",
+                      "tv newscaster",
+                      "tv voice",
+                      "tvvoice",
+                      "voice",
+                      "voice 1",
+                      "voice 2",
+                      "voice from poker game",
+                      "voice on intercom",
+                      "voice on speaker",
+                      "voice on the phone",
+                      "woman's voice on the phone"
+                      ]
 
 replacements = {"pitt": "mr. pitt",
                 "steinbrenner": "mr. steinbrenner",
                 "lippman": "mr. lippman",
                 "mrs. costanza": "estelle",
                 "devola": "joe divola",
+                "davola": "joe divola",
                 "leo": "uncle leo",
                 "mr ross": "mr. ross",
                 "j\. peterman": "peterman",
                 "mr. peterman": "peterman",
                 "claie": "claire",
-                "marry": "mary"}
+                "marry": "mary",
+                "allsion": "allison",
+                "mr\.thomassoulo": "thomassoulo",
+                "mr\. thomassoulo": "thomassoulo",
+                "babu bhatt": "babu",
+                "docter": "doctor",
+                "dry cleane": "dry cleaner",
+                "elainelaine": "elaine",
+                "eliane": "elaine",
+                "goerge": "george",
+                "izzy izzy sr\.\.": "izzy sr.",
+                "izzy": "izzy sr.",
+                "jerr": "jerry",
+                "jery": "jerry",
+                "krmaer": "kramer",
+                "man 1": "man #1",
+                "man 2": "man #2",
+                "man 3": "man #3",
+                "man#1": "man #1",
+                "man#2": "man #2",
+                "man#3": "man #3",
+                "man1": "man #1",
+                "man2": "man #2",
+                "man3": "man #3",
+                "marcellino": "marcelino",
+                "newmanewman": "newman",
+                "whatley": "tim"
+                }
 
+seinfeld_df = remove_speakers(seinfeld_df, speakers_to_remove)
+
+seinfeld_df = fix_filtered_names(seinfeld_df, [[4, 17], [4, 21], [4, 24]], {"allison": "allison (season 4)"})
+seinfeld_df = fix_filtered_names(seinfeld_df, [[8, 15]], {"allison": "allison (season 8)"})
+seinfeld_df = fix_filtered_names(seinfeld_df, [[9, 2]], {"allison": "allison from play now"})
+seinfeld_df = fix_filtered_names(seinfeld_df, [[7, 18]], {"bob": "bob grossberg"})
+seinfeld_df = fix_filtered_names(seinfeld_df, [[5, 15]], {"bob": "bob from suit store"})
+seinfeld_df = fix_filtered_names(seinfeld_df, [[8, 19]], {"father": "father curtis"})
 
 seinfeld_df['speaker'] = seinfeld_df.speaker.apply(fix_names, args=(replacements,))
+seinfeld_df = distinguish_characters(seinfeld_df, ["cashier", "cop", "doctor", "doorman", "guy", "judge", "kid", "man",
+                                                   "man #1", "man #2", "man #3", "waiter", "waitress", "woman", "worker"])
 seinfeld_df.groupby("speaker").size().reset_index(name="count").sort_values("count", ascending=False)[:100]
 seinfeld_df.speaker.nunique()
 
@@ -40,7 +124,7 @@ seinfeld_df.to_csv("../data/seinfeld/seinfeld_lines_v2.csv", index=False, encodi
 
 
 seinfeld_df.groupby("speaker").size().reset_index(name="count").sort_values("count", ascending=False)
-seinfeld_df.speaker.nunique()   # 992
+seinfeld_df.speaker.nunique()   # 1116
 
 lines_by_season = seinfeld_df.groupby('season')['line'].count()
 lines_by_season.plot.bar(title="Number of lines by season", ylabel="Number of lines")
