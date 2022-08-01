a08e06ecb34cab56217d8aa8c9a18e5ea1d8fd37
WojtekBogucki <w.bogucki@student.mini.pw.edu.pl>
2022-08-01 22:11:48 +0200

    Add functions for movies and upgraded plots

--- networks/movie_networks.py
+++ networks/movie_networks.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 from utils import *
 import os
@@ -10,45 +9,47 @@ pd.options.display.max_columns = 30
 
 matplotlib.use('Agg')
 
-path_dir = "../data/movies"
-movie_paths = [os.path.join(path_dir, movie) for movie in os.listdir(path_dir)]
-movie_titles = [movie.split(".")[0] for movie in os.listdir(path_dir)]
-
-os.makedirs("../figures/movies", exist_ok=True)
-
 selected_movies = ["batman", "blade_runner", "braveheart", "citizen_kane", "dead_poets_society", "die_hard", "fargo",
                    "good_will_hunting", "hannibal", "independence_day", "jaws_2", "jurassic_park",
                    "monty_python_and_the_holy_grail", "pirates_of_the_caribbean", "saving_private_ryan", "scream",
                    "spider-man", "superman", "the_big_lebowski", "the_bourne_identity",
                    "the_godfather", "the_matrix", "titanic", "tomorrow_never_dies"]
 
-i = 0
-for movie_path, movie_title in zip(movie_paths[i:], movie_titles[i:]):
-    if movie_title in selected_movies:
-        print(movie_title)
-        edges_weighted = pd.read_csv(movie_path)
-        net = nx.from_pandas_edgelist(edges_weighted, source="speaker1", target="speaker2",
-                                      edge_attr=["line_count", "scene_count", "word_count"])
-        draw_interaction_network_communities(net, "line_count", method="LD", filename=f"movies/{movie_title}_lines")
-        # draw_interaction_network_communities(net, "scene_count", method="LD", filename=f"movies/{movie_title}_scenes")
-        # draw_interaction_network_communities(net, "word_count", method="LD", filename=f"movies/{movie_title}_words")
+
+def draw_movies_networks(selected: list, path_dir: str = "../data/movies") -> None:
+    os.makedirs("../figures/movies", exist_ok=True)
+    paths = [os.path.join(path_dir, movie) for movie in os.listdir(path_dir)]
+    titles = [movie.split(".")[0] for movie in os.listdir(path_dir)]
+    for movie_path, movie_title in zip(paths, titles):
+        if movie_title in selected:
+            print(movie_title)
+            edges_weighted = pd.read_csv(movie_path)
+            net = nx.from_pandas_edgelist(edges_weighted, source="speaker1", target="speaker2",
+                                          edge_attr=["line_count", "scene_count", "word_count"])
+            draw_interaction_network_communities(net, "line_count", method="LD", filename=f"movies/{movie_title}_lines")
+            # draw_interaction_network_communities(net, "scene_count", method="LD", filename=f"movies/{movie_title}_scenes")
+            # draw_interaction_network_communities(net, "word_count", method="LD", filename=f"movies/{movie_title}_words")
 
 
+def get_movies_networks(selected: list, path_dir: str = "../data/movies") -> list:
+    paths = [os.path.join(path_dir, movie) for movie in os.listdir(path_dir)]
+    titles = [movie.split(".")[0] for movie in os.listdir(path_dir)]
+    movies_net = []
+    for movie_path, movie_title in zip(paths, titles):
+        if movie_title in selected:
+            print(movie_title)
+            edges_weighted = pd.read_csv(movie_path)
+            net = nx.from_pandas_edgelist(edges_weighted, source="speaker1", target="speaker2",
+                                          edge_attr=["line_count", "scene_count", "word_count"])
+            movies_net.append(net)
+    return movies_net
 
-movies_net = []
-for movie_path, movie_title in zip(movie_paths, movie_titles):
-    if movie_title in selected_movies:
-        print(movie_title)
-        edges_weighted = pd.read_csv(movie_path)
-        net = nx.from_pandas_edgelist(edges_weighted, source="speaker1", target="speaker2",
-                                      edge_attr=["line_count", "scene_count", "word_count"])
-        movies_net.append(net)
 
+movies_net = get_movies_networks(selected_movies)
 movie_stats = get_movie_network_stats(movies_net, selected_movies)
 
 
-create_similarity_matrix(movie_stats, selected_movies, filename="comparison/movies")
-
+# create_similarity_matrix(movie_stats, selected_movies, filename="comparison/movies")
 
 # load serials
 episode_stats = pd.DataFrame()
@@ -73,6 +74,7 @@ dists = pdist(norm_all_stats.fillna(0), "euclidean")
 df_euclid = pd.DataFrame(1 / (1 + squareform(dists)), columns=norm_all_stats.index,
                          index=norm_all_stats.index)
 
+# the most similar
 most_similar = pd.DataFrame()
 for movie in selected_movies:
     most_sim = df_euclid.iloc[:24, :].transpose().loc[:, movie].sort_values(ascending=False)[1:11]
@@ -82,6 +84,7 @@ most_sim_count = most_similar.notna().sum(axis=1).sort_values(ascending=False).h
 
 most_sim_count.index = most_sim_count.index.str.replace("_", " ").str.capitalize()
 most_sim_count = most_sim_count.to_frame(name="count")
+print(most_sim_count)
 
 # similarity - movie vs series
 most_similar_melt = df_euclid.iloc[:24, 24:].reset_index().melt(id_vars=["index"], var_name="tv_series", value_name="similarity")
@@ -89,7 +92,7 @@ most_similar_melt["index"] = most_similar_melt["index"].str.replace("_", " ").st
 most_similar_melt["tv_series"] = most_similar_melt["tv_series"].str.replace("_", " ").str.capitalize()
 most_similar_melt.sort_values("similarity", ascending=False).round(3).head(10)
 
-
+# movies and serials average similarity
 df_euclid.iloc[:24, :].transpose().mean()
 movies_similarities = (df_euclid.iloc[:24, :24].transpose().sum()-1)/24
 
@@ -101,24 +104,34 @@ pd.concat([movies_similarities, serials_similarities], axis=1)
 avg_similarity = (df_euclid.sum(axis=1)-1)/(df_euclid.shape[0]-1)
 
 most_unique = avg_similarity.sort_values(ascending=True).round(3).head(10)
-most_unique.index = most_unique.index .str.replace("_", " ").str.capitalize()
+indexes = []
+for show_name in most_unique.index.values:
+    indexes.append(norm_all_stats.reset_index()[norm_all_stats.index.str.startswith(show_name)].index.values[0])
+
+most_unique.index = most_unique.index.str.split("_").map(lambda x: " ".join(e.capitalize() for e in x))
 
 
 matplotlib.use('Tkagg')
 
-show_name = "seinfeld"
-net_episodes = get_episode_networks_limit(f"../data/{show_name}/", 5)
-latest_file = [f for f in os.listdir(f"../data/{show_name}/") if f.startswith(f"{show_name}_lines_v")][-1]
-episode_dict = get_episode_dict(f"../data/{show_name}/{latest_file}")
-
-draw_interaction_network_communities(net_episodes[episode_dict["s06e19"]], "line_count", method="LD", seed=777, filename=f"{show_name}/episode_networks/s06e19_line_LD_limit5")
+# show_name = "seinfeld"
+# net_episodes = get_episode_networks_limit(f"../data/{show_name}/", 5)
+# latest_file = [f for f in os.listdir(f"../data/{show_name}/") if f.startswith(f"{show_name}_lines_v")][-1]
+# episode_dict = get_episode_dict(f"../data/{show_name}/{latest_file}")
+#
+# draw_interaction_network_communities(net_episodes[episode_dict["s09e11"]], "line_count", method="LD", seed=777, filename=f"{show_name}/episode_networks/s09e11_line_LD_limit5")
 
 # PCA
 pca = PCA(n_components=2)
-all_stats_pca = pca.fit_transform(norm_all_stats.dropna(axis=1))
+print(norm_all_stats.isna().sum())
+norm_all_stats_clean = norm_all_stats.dropna(axis=1)
+all_stats_pca = pca.fit_transform(norm_all_stats_clean)
 loadings = pd.DataFrame(pca.components_, columns=pca.feature_names_in_, index=["Component 1", "Component 2"])
 
-loadings.transpose().plot(kind="barh", layout=(1,2), subplots=True, sharey=True, legend=False)
+fig, ax = plt.subplots(figsize=(8,5))
+loadings.transpose().plot(kind="barh", layout=(1, 2), subplots=False, sharey=True, legend=True, ax=ax)
+plt.xlabel("Loadings")
+plt.tight_layout()
+plt.savefig("../figures/comparison/loadings_all")
 
 print(pca.explained_variance_ratio_)
 
@@ -126,34 +139,31 @@ show_idx = dict()
 for show_name in ["the_office", "seinfeld", "tbbt", "friends"]:
     show_idx[show_name] = norm_all_stats.reset_index()[norm_all_stats.index.str.startswith(show_name)].index.values
 
-indexes = []
-for show_name in most_unique.index.values:
-    indexes.append(norm_all_stats.reset_index()[norm_all_stats.index.str.startswith(show_name)].index.values[0])
 
-np.argwhere(norm_all_stats.index.str.startswith("seinfeld"))
-np.argwhere(norm_all_stats.index.str.startswith("the_matrix"))
-norm_all_stats.index[np.argwhere((all_stats_pca[:,0]>6) & (all_stats_pca[:,1]>7))]
+# np.argwhere(norm_all_stats.index.str.startswith("seinfeld"))
+# np.argwhere(norm_all_stats.index.str.startswith("the_matrix"))
+# norm_all_stats.index[np.argwhere((all_stats_pca[:,0]>6) & (all_stats_pca[:,1]>7))]
+
+show_names = {
+    "the_office": "The Office",
+    "seinfeld": "Seinfeld",
+    "tbbt": "The Big Bang Theory",
+    "friends": "Friends"
+}
 
 alpha = 0.8
 s = 50
 plt.style.use('default')
-fig, ax = plt.subplots(figsize=(14, 7))
+_, ax = plt.subplots(figsize=(16, 8))
 for show_name in ["the_office", "seinfeld", "tbbt", "friends"]:
-    ax.scatter(all_stats_pca[show_idx[show_name], 0], all_stats_pca[show_idx[show_name], 1], label=f"{show_name} episodes", alpha=alpha, s=s)
+    ax.scatter(all_stats_pca[show_idx[show_name], 0], all_stats_pca[show_idx[show_name], 1], label=f"{show_names[show_name]} episodes", alpha=alpha, s=s)
 ax.scatter(all_stats_pca[:24, 0], all_stats_pca[:24, 1], label="Movies", alpha=alpha, s=s)
-# ts = []
-# for x, y, text in zip(all_stats_pca[:24, 0], all_stats_pca[:24, 1], selected_movies):
-#     ts.append(plt.text(x, y, text))
-# adjust_text(ts, force_points=0.1, arrowprops=dict(arrowstyle='->', color='red'))
-text = [plt.text(all_stats_pca[13, 0], all_stats_pca[13, 1], "Pirates of the Caribbean", fontsize=12),
-        plt.text(all_stats_pca[309, 0], all_stats_pca[309, 1], "Seinfeld s06e19", fontsize=12),
-        plt.text(all_stats_pca[21, 0], all_stats_pca[21, 1], "The Matrix", fontsize=12)]
-text = text + [plt.text(all_stats_pca[idx, 0], all_stats_pca[idx, 1], title, fontsize=12) for title, idx in zip(most_unique.index.values, indexes)]
+text = [plt.text(all_stats_pca[idx, 0], all_stats_pca[idx, 1], title, fontsize=12) for title, idx in zip(most_unique.index.values, indexes)]
 ax.legend()
-plt.title("Principal component analysis of statistics of movies and TV series")
-plt.xlabel(f"Principal component 1 ({100*pca.explained_variance_ratio_[0]:.2f} % explained variance)")
-plt.ylabel(f"Principal component 2 ({100*pca.explained_variance_ratio_[1]:.2f} % explained variance)")
+plt.title("Principal component analysis of statistics of movies and TV series", fontsize=16)
+plt.xlabel(f"Principal component 1 ({100*pca.explained_variance_ratio_[0]:.2f} % explained variance)", fontsize=12)
+plt.ylabel(f"Principal component 2 ({100*pca.explained_variance_ratio_[1]:.2f} % explained variance)", fontsize=12)
 adjust_text(text, x=all_stats_pca[:, 0], y=all_stats_pca[:, 1], arrowprops=dict(arrowstyle='->', color='red'))
-plt.show()
+plt.savefig("../figures/comparison/pca_all")
 
 
